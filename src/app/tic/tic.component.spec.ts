bec3efae77c96406ce603b664d864b6f4c1bfaca
Xiaoxuan <freyajiang0814@gmail.com>
2019-04-06 16:58:35 -0700

    Add test

--- src/app/tic/tic.component.spec.ts
+++ src/app/tic/tic.component.spec.ts
@@ -22,4 +22,30 @@ describe('TicComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should interact when clicked', () => {
+    component.onClickMe(0);
+    expect(component.turns[0]).toBe('X');
+  });
+
+  it('should change player at different turns', () => {
+    component.onClickMe(0);
+    component.onClickMe(1);
+    expect(component.turns[1]).toBe('O');
+  });
+
+  it('should disable repetitive clicks', () => {
+    component.onClickMe(0);
+    component.onClickMe(0);
+    expect(component.turns[0]).toBe('X');
+  });
+
+  it('should allow reset', () => {
+    component.onClickMe(0);
+    component.onClickMe(1);
+    component.reset();
+    expect(component.turns[0]).toBe('');
+    expect(component.turns[1]).toBe('');
+  });
+
 });
