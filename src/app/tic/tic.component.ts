6f89ba01d744342b92c8564a84bc0820ab6be2e0
Xiaoxuan <freyajiang0814@gmail.com>
2019-04-06 18:18:29 -0700

    Created Winning Condition checker

--- src/app/tic/tic.component.ts
+++ src/app/tic/tic.component.ts
@@ -17,6 +17,10 @@ export class TicComponent implements OnInit {
   turns: string[];
   status = 'O';
 
+  // TODO: make it a constant
+  winConditions: string[] = ["012", "345", "678","036","147","258","048","246"];
+  gameEnd: boolean = false;
+
   ngOnInit() {
   }
 
@@ -31,9 +35,19 @@ export class TicComponent implements OnInit {
       this.status = 'X';
       this.turns[buttonIndex] = this.status;
     }
+    this.isWin(this.winConditions, this.status);
   }
 
   reset() {
     this.turns = ['', '', '', '', '', '', '', '', ''];
+  }
+
+  isWin(winConditions, status) {
+    for (var winCondition of winConditions) {
+      if ((this.turns[winCondition.charAt(0)] === this.turns[winCondition.charAt(1)]) && (this.turns[winCondition.charAt(1)] === this.turns[winCondition.charAt(2)]) && this.turns[winCondition.charAt(0)] !== "") {
+         console.log( status + " wins");
+         this.gameEnd = true;
+      }
+    }
   }
 }
